b5b4df1505ce36a0e4add6850aa77a45fc2e5b7a
dazaiii <kin.baryczka@gmail.com>
2021-03-22 14:40:21 +0100

    Creating entity manager

---
+++ src/main/java/manager/EntityManager.java
@@ -0,0 +1,48 @@
+package manager;
+
+import entity.HibernateFactory;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.criteria.CriteriaBuilder;
+
+public class EntityManager {
+    public boolean add(Object object){
+        HibernateFactory hibernateFactory = new HibernateFactory();
+        Session session = hibernateFactory.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        boolean isAdded = false;
+        try {
+            session.save(object);
+            session.getTransaction().commit();
+            isAdded = true;
+        } catch(Exception e){
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+            hibernateFactory.getSessionFactory().close();
+        }
+        return isAdded;
+    }
+
+    public boolean delete(Object object){
+        HibernateFactory hibernateFactory = new HibernateFactory();
+        Session session = hibernateFactory.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        boolean isDeleted = false;
+        try {
+            session.delete(object);
+            session.getTransaction().commit();
+            isDeleted = true;
+        } catch(Exception e){
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+            hibernateFactory.getSessionFactory().close();
+        }
+        return isDeleted;
+    }
+
+}
--- src/main/java/manager/StudentManager.java
+++ src/main/java/manager/StudentManager.java
@@ -10,49 +10,13 @@ import java.util.List;
 
 public class StudentManager {
     public boolean addStudent(Student student){
-        HibernateFactory hibernateFactory = new HibernateFactory();
-        Session session = hibernateFactory.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        boolean isAdded;
-        System.out.println("Transaction started");
-        try {
-            session.save(student);
-            session.getTransaction().commit();
-            System.out.println("Student added");
-            isAdded = true;
-        } catch(Exception e){
-            transaction.rollback();
-            e.printStackTrace();
-            System.out.println("Student cannot be added");
-            isAdded = false;
-        } finally {
-            session.close();
-            hibernateFactory.getSessionFactory().close();
-        }
-        return isAdded;
+        EntityManager entityManager = new EntityManager();
+        return entityManager.add(student);
     }
 
     public boolean deleteStudent(Student student){
-        HibernateFactory hibernateFactory = new HibernateFactory();
-        Session session = hibernateFactory.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        System.out.println("Transaction started");
-        boolean isDeleted;
-        try {
-            session.delete(student);
-            session.getTransaction().commit();
-            System.out.println("Student deleted");
-            isDeleted = true;
-        } catch(Exception e){
-            transaction.rollback();
-            e.printStackTrace();
-            System.out.println("Student cannot be deleted");
-            isDeleted = false;
-        } finally {
-            session.close();
-            hibernateFactory.getSessionFactory().close();
-        }
-        return isDeleted;
+        EntityManager entityManager = new EntityManager();
+        return entityManager.delete(student);
     }
 
     public Student findById(Long id){
@@ -85,14 +49,20 @@ public class StudentManager {
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
         CriteriaQuery<Student> criteriaQuery = criteriaBuilder.createQuery(Student.class);
         Root<Student> root = criteriaQuery.from(Student.class);
+
         Predicate[] predicates = new Predicate[2];
         predicates[0] = criteriaBuilder.like(root.get("surname"), "%"+surname+"%");
         predicates[1] = criteriaBuilder.like(root.get("name"), "%"+name+"%");
+
         criteriaQuery.select(root).where(predicates);
         Query<Student> query = session.createQuery(criteriaQuery);
         List<Student> results = query.getResultList();
+
         for(Student i : results)
             System.out.println(i);
+
+        session.close();
+        hibernateFactory.getSessionFactory().close();
         return results;
     }
 
@@ -102,17 +72,26 @@ public class StudentManager {
         CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
         CriteriaQuery<Student> criteriaQuery = criteriaBuilder.createQuery(Student.class);
         Root<Student> root = criteriaQuery.from(Student.class);
+
         if(type.equalsIgnoreCase("asc")){
             criteriaQuery.orderBy(criteriaBuilder.asc(root.get(column)));
         } else if(type.equalsIgnoreCase("desc")) {
             criteriaQuery.orderBy(criteriaBuilder.desc(root.get(column)));
         }
+
         criteriaQuery.orderBy(criteriaBuilder.asc(root.get(column)));
         Query<Student> query = session.createQuery(criteriaQuery);
         List<Student> results = query.getResultList();
-        System.out.println("Sortowanie");
+
         for(Student i : results)
             System.out.println(i);
+
+        session.close();
+        hibernateFactory.getSessionFactory().close();
         return results;
+    }
+
+    public void addGrade(){
+
     }
 }
