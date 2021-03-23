fc3c7ea52c603449da10602a85b278df5b502d66
dazaiii <kin.baryczka@gmail.com>
2021-03-23 14:09:33 +0100

    Grade and class manager

--- src/main/java/manager/ClassManager.java
+++ src/main/java/manager/ClassManager.java
@@ -1,4 +1,33 @@
 package manager;
 
+import entity.Student;
+import entity.Class;
+
+import java.util.List;
+
 public class ClassManager {
+    public boolean addClass(Class schoolClass){
+        EntityManager entityManager = new EntityManager();
+        return entityManager.add(schoolClass);
+    }
+
+    public boolean deleteClass(Class schoolClass){
+        EntityManager entityManager = new EntityManager();
+        return entityManager.delete(schoolClass);
+    }
+
+    public Class updateClass(Class schoolClass){
+        EntityManager entityManager = new EntityManager();
+        entityManager.update(schoolClass);
+        return schoolClass;
+    }
+
+    public Class addStudent(Class schoolClass, Student student){
+        List<Student> list = schoolClass.getStudents();
+        list.add(student);
+        schoolClass.setStudents(list);
+        EntityManager entityManager = new EntityManager();
+        entityManager.update(schoolClass);
+        return schoolClass;
+    }
 }
--- src/main/java/manager/GradeManager.java
+++ src/main/java/manager/GradeManager.java
@@ -1,4 +1,50 @@
 package manager;
 
+import entity.Grade;
+import entity.Student;
+import entity.Teacher;
+import java.util.List;
+
 public class GradeManager {
+    public boolean addGrade(Grade grade){
+        boolean isAdded = false;
+        if(grade.getStudent() != null && grade.getTeacher() != null){
+            EntityManager entityManager = new EntityManager();
+            isAdded = entityManager.add(grade);
+        }
+        return isAdded;
+    }
+
+    public boolean deleteGrade(Grade grade){
+        boolean isDeleted = true;
+        if(grade.getStudent() != null && grade.getTeacher() != null){
+            EntityManager entityManager = new EntityManager();
+            isDeleted = entityManager.delete(grade);
+        }
+        return isDeleted;
+    }
+
+    public Grade updateGrade(Grade grade, int newGrade){
+        grade.setGrade(newGrade);
+        EntityManager entityManager = new EntityManager();
+        entityManager.update(grade);
+        return grade;
+    }
+
+    public double averageGrades(Student student, Teacher teacher){
+        List<Grade> list = student.getGrades();
+        if(list == null){
+            return 0;
+        }
+        double avg = 0;
+        int counter = 0;
+        for(Grade i: list){
+            if(i.getTeacher().getSubject().equals(teacher.getSubject())){
+                avg += i.getGrade();
+                counter++;
+            }
+        }
+        avg = avg / counter;
+        return avg;
+    }
 }
